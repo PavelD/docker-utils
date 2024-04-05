2a9ae42fbc598daba5a56b17092859aeba8f3989
PavelD <konference@lnx.cz>
2024-04-05 21:58:19 +0200

    add error + usage

--- find parent/find_parent.sh
+++ find parent/find_parent.sh
@@ -2,6 +2,16 @@
 
 set -euo pipefail
 
+if [ "$#" -ne 1 ]; then
+  echo "ERROR: Illegal number of parameters"
+  echo
+  echo "Find docker image parent"
+  echo
+  echo "Usage:"
+  echo "   $0 DOCKER_IMAGE_ID"
+  exit 1
+fi
+
 parent=$(docker inspect -f '{{.Id}}' $1)
 
 for id in `docker inspect -f '{{.Id}} {{.Parent}}' $(docker images -a -f since=$parent -q) | awk -v parent=$parent '{ if($2 == parent) print $1 }'`; do
