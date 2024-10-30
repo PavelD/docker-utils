a87be0e287ae3adc0aa22303c401c73c513574c3
PavelD <konference@lnx.cz>
2024-10-30 03:37:06 +0100

    fix recursive call

--- find-dependent-child-images/find_child.sh
+++ find-dependent-child-images/find_child.sh
@@ -15,6 +15,6 @@ fi
 parent=$(docker inspect -f '{{.Id}}' "$1")
 
 for id in $(docker inspect -f '{{.Id}} {{.Parent}}' $(docker images -a -f since="$parent" -q) | awk -v parent="$parent" '{ if($2 == parent) print $1 }'); do
-  $0 $id
+  "$0" "$id"
   docker images | grep $(echo "$id" | cut -c 8-19)
 done
